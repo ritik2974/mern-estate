a1b8310a70a4ef7af54a031751ef3898d4d6047b
ritik2974 <ritik2974@gmail.com>
2024-05-29 00:51:05 +0530

    create user mode

---
+++ api/models/user.mode.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true}); //used in sorting on basis of time
+
+
+
+const User =mongoose.model('Usser',userSchema);
+
+export default User;
