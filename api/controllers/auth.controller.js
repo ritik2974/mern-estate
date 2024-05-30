0e38bd13da39d45d593fed8dc108083dd1ef2e04
ritik2974 <ritik2974@gmail.com>
2024-05-31 00:08:09 +0530

    middlewares

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup=async(req,res)=>{
+import { errorHandler } from "../utils/error.js";
+export const signup=async(req,res,next)=>{
 const {username,email,password}=req.body;
 const hashedPassword=bcryptjs.hashSync(password,10);
 const newUser=new User({username,email,password:hashedPassword});
@@ -11,7 +12,7 @@ await newUser.save();// for saving in database,first declare any variable ,the v
 //here await is used as this operation takes time,by using await code will stay in this line till this operation is completed,then we go to next line and if await is used then async will also be used  
 res.status(201).json("User created succesfully");
 } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
 }
 
 
