c4b7820baa3c7347cc28542db5339ccde022718d
ritik2974 <ritik2974@gmail.com>
2024-06-06 21:45:02 +0530

    redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import {persistReducer,persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+// here persistReducer and storage and root reducer are 
+// used to store current user in browser storage as after
+//  refreshing the page we loose our current user as it 
+//  get sets to null
+
+const rootReducer=combineReducers({user:userReducer})
+
+const persistConfig={
+    key:'root',
+    storage,
+    version:1,
+}
+
+
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+
+
 export const store = configureStore({
-  reducer: {user:userReducer}, 
+  reducer: persistedReducer, 
   middleware:(getDefaultMiddleware)=>
     getDefaultMiddleware({
         serializableCheck:false,
@@ -9,3 +31,5 @@ export const store = configureStore({
     // middleware is just used here to remove errors in the browser//
  
 }) 
+
+export const persistor=persistStore(store);
