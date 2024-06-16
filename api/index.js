2f892fc4c9d68a0394c2eb817be7dd6e119e44b8
ritik2974 <ritik2974@gmail.com>
2024-06-16 15:11:06 +0530

    user update functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDb!');
@@ -13,7 +14,9 @@ console.log(err);
 
 const app=express();
 
-app.use(express.json()); 
+app.use(express.json());
+ 
+app.use(cookieParser());
 
 app.listen(3000,()=>{
     console.log('server running on port 3000');
@@ -21,7 +24,6 @@ app.listen(3000,()=>{
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
-
 app.use((err,req,res,next)=>{
 const statusCode=err.statusCode|| 500;
 const message=err.message|| 'Internal Server Error';
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -25,10 +25,22 @@ const userSlice=createSlice({
         signInFailure:(state,action)=>{
             state.error=action.payload;
             state.loading=false;
-        }
-    }
+        },
+        updateUserStart:(state)=>{
+            state.loading=true;
+        },
+        updateUserSuccess:(state,action)=>{
+            state.currentUser=action.payload;
+            state.loading=false;
+            state.error=null;
+        },
+        updateUserFailure:(state,action)=>{
+            state.error=action.payload;
+            state.loading=false;
+        },
+    },
 })
 
-export const {signInstart,signInSuccess,signInFailure}=userSlice.actions;
+export const {signInstart,signInSuccess,signInFailure,updateUserStart,updateUserSuccess,updateUserFailure}=userSlice.actions;
 
 export default userSlice.reducer;
