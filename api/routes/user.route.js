e7d22f09c97a36531a51f6c2f4729ed77d9878ad
ritik2974 <ritik2974@gmail.com>
2024-06-18 23:29:09 +0530

    complete route for user listings and show user listings functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { deleteUser, test } from '../controllers/user.controller.js';
-import { updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test,updateUser,getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router=express.Router();
 
@@ -8,5 +7,5 @@ const router=express.Router();
 router.get('/test',test)
 router.post('/update/:id',verifyToken,updateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
-
+router.get('/listings/:id',verifyToken,getUserListings);
 export default router;
