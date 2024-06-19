1f861213e293da2faf2537299f1a560fdff9ca3b
ritik2974 <ritik2974@gmail.com>
2024-06-19 11:57:35 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -29,6 +29,7 @@ export const deleteListing=async(req,res,next)=>{
 
 export const updateListing=async(req,res,next)=>{
    const listing=await Listing.findById(req.params.id);
+   
    if(!listing){
       return next(errorHandler(404,'Listing not found'));
    }
@@ -37,10 +38,22 @@ export const updateListing=async(req,res,next)=>{
    }
    try{  
       const updatedListing=await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
-
-      res.status(200).json(updateListing);
+      
+      res.status(200).json(updatedListing);
    }
    catch(error){
+      next(error);
+   }
+}
+
+export const getListing=async(req,res,next)=>{
+   try {
+      const listing=await Listing.findById(req.params.id);
+      if(!listing){
+         return next(errorHandler(404,'Listing not found'));
+      } 
+      res.status(200).json(listing);
+   } catch (error) {
       next(error);
    }
 }
