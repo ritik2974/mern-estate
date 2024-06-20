9b6155ed9b6884763593c39101441c12cfc99f50
ritik2974 <ritik2974@gmail.com>
2024-06-20 11:44:23 +0530

    add contact landlord functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,57 +1,3 @@
-// import  { useEffect,useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import {Swiper,SwiperSlide} from 'swiper/react'
-// import SwiperCore from 'swiper'
-// import {Navigation} from 'swiper/modules'
-// import 'swiper/css/bundle'
-// export default function Listing() {
-//     SwiperCore.use(Navigation);
-//     const [listing,setListing]=useState(null);
-//     const[loading,setLoading]=useState(false);
-//     const [error,setError]=useState(false);
-//     const params=useParams();
-//     useEffect (()=>{
-//        const fetchListing =async()=>{
-//         try{
-//             setLoading(true);
-//         const res=await fetch(`/api/listing/get/${params.listingId}`);
-//         const data=await res.json();
-//         if(data.success===false){
-//             setError(true);
-//             setLoading(false);
-//             return ;
-//         }
-//         setListing(data);
-//         setLoading(false);
-//         setError(false);
-//     }
-//     catch(error){
-//         setError(true);
-//         setLoading(false);
-//     }
-//        }
-//        fetchListing();
-//     },[params.listingId])
-//   return (
-//   <main>
-//     {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
-//     {error && (<p className='text-center my-7 text-2xl'>Something went wrong!</p>)}
-//     {listing && !loading && !error && (
-//         <div>
-//     <Swiper navigation>
-//         {listing.imageUrls.map((url)=>(
-            
-//             <SwiperSlide key={url}>
-//             <div 
-//             className='h-[550px]' style=
-//             {{background:`url(${url}) center no-repeat`,backgroundSize:'cover'}}></div>
-//             </SwiperSlide>))}
-//     </Swiper>
-//     </div>
-//     )}
-//     </main>
-// )
-// }
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -78,6 +24,7 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact,setContact]=useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
 
@@ -189,7 +136,11 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            
+            {currentUser && listing.userRef!==currentUser._id && !contact &&( 
+            <button onClick={()=>setContact(true)}className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+              Contact landlord</button>
+              )}
+              {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
